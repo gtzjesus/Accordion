d5d036d15522454a098c26d00fc107a231f0bb19
gtz.jesus <gtz.jesus@outlook.com>
2023-08-07 17:28:54 -0600

    Data Flow Children, Done

--- src/App.js
+++ src/App.js
@@ -25,32 +25,45 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [currentOpen, setCurrentOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((element, index) => (
         <AccordionItem
+          currentOpen={currentOpen}
+          onOpen={setCurrentOpen}
           title={element.title}
-          text={element.text}
           num={index}
           key={element.title}
-        />
+        >
+          {element.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        currentOpen={currentOpen}
+        onOpen={setCurrentOpen}
+        title="test"
+        num={22}
+        key="tester"
+      >
+        <p>ALLOWS REACT DEVS TO</p>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
+function AccordionItem({ num, title, currentOpen, onOpen, children }) {
+  const isOpen = num === currentOpen;
   function handleToggle() {
-    setIsOpen((current) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
   return (
     <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
